c13b65ee7c86047df75dd5e34b58be6ac02ba867
Joao Lopes <jandrevl@gmail.com>
2022-09-03 00:57:49 +0100

    First Spinner installed

--- app/src/main/java/com/jandrevl/thecurrencyconverter/MainActivity.java
+++ app/src/main/java/com/jandrevl/thecurrencyconverter/MainActivity.java
@@ -3,12 +3,26 @@ package com.jandrevl.thecurrencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner baseCurrenciesSpinner = findViewById(R.id.baseCurrencySpinner);
+
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.baseCurrencies, android.R.layout.simple_spinner_item);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        baseCurrenciesSpinner.setAdapter(arrayAdapter);
+
+
     }
 }
