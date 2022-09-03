0ad3688ce2be7ec02773caf52a84845bbbcf4976
Joao Lopes <jandrevl@gmail.com>
2022-09-03 20:28:18 +0100

    Second spinner added. Figured onItemSelected method

--- app/src/main/java/com/jandrevl/thecurrencyconverter/MainActivity.java
+++ app/src/main/java/com/jandrevl/thecurrencyconverter/MainActivity.java
@@ -3,13 +3,15 @@ package com.jandrevl.thecurrencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
+    String baseCurrency;
+    String currency;
 
 
     @Override
@@ -18,11 +20,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Spinner baseCurrenciesSpinner = findViewById(R.id.baseCurrencySpinner);
+        Spinner currenciesSpinner = findViewById(R.id.currencySpinner);
 
-        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.baseCurrencies, android.R.layout.simple_spinner_item);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        baseCurrenciesSpinner.setAdapter(arrayAdapter);
+        ArrayAdapter<CharSequence> baseCurrenciesArrayAdapter = ArrayAdapter.createFromResource(this, R.array.baseCurrencies, android.R.layout.simple_spinner_item);
+        baseCurrenciesArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        baseCurrenciesSpinner.setAdapter(baseCurrenciesArrayAdapter);
 
+        ArrayAdapter<CharSequence> currenciesArrayAdapter = ArrayAdapter.createFromResource(this, R.array.currencies, android.R.layout.simple_spinner_item);
+        currenciesArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        currenciesSpinner.setAdapter(currenciesArrayAdapter);
+
+        baseCurrenciesSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                baseCurrency = getResources().getStringArray(R.array.baseCurrencies)[i];
+                Toast.makeText(MainActivity.this, baseCurrency, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                baseCurrency = "EUR";
+
+            }
+        });
 
     }
 }
